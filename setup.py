15947921b3b4cbaf55051383737ef0f75c428b5f
Shah <sh.abedinnejad@gmail.com>
2026-02-07 14:12:51 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read() 
 
 setuptools.setup(
-    name="src",
-    version="0.0.0",
+    name="drinks-quality",
+    version="0.1.0",
     author="Shah Abedinn",
     author_email="abedinn.shah@gmail.com",
-    install_requires=[
-    ],
+    description="A package for Drinks Quality Prediction",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages()
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.10",
+       install_requires=[
+    ],
 )
 
